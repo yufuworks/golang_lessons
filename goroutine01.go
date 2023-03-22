a07b0d1f9c2dfb0085d1f9972e5a862c80028f54
yufuworks <yufuworks@outlook.jp>
2023-03-22 22:38:13 +0900

    ss6: Goroutine 52. channel

---
+++ goroutine01.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+func goroutine2(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int)
+	go goroutine1(s, c)
+	go goroutine2(s, c)
+	x := <-c
+	fmt.Println(x)
+	y := <-c
+	fmt.Println(y)
+}
