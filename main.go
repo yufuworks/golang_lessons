5645f21acc2f99fd52c3631b8f00ce970d355399
yufuworks <yufuworks@outlook.jp>
2023-04-02 23:51:43 +0900

    ss7: Package 66. サードパーティーのpackageのインストール

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"awesomeProject/mylib/lessons/s7_package"
 	"awesomeProject/mylib/lessons/s7_package/under"
 	"fmt"
+
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
 )
 
 func main() {
@@ -18,4 +21,13 @@ func main() {
 	person.SetHeight(180)
 	fmt.Println(person)
 	fmt.Println(under.Public)
+
+	/*
+		https://github.com/markcheno/go-talib
+		Exampleに従ってコピペ、試用
+	*/
+	spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
+	fmt.Print(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
 }
