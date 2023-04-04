a6aef42e43e596840a2e83e66bb38a25a667ac5d
yufuworks <yufuworks@outlook.jp>
2023-04-04 21:12:58 +0900

    ss8: 便利なパッケージ 68. regex

---
+++ mylib/lessons/s8/regex.go
@@ -0,0 +1,25 @@
+package s8
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func Regex() {
+	match, _ := regexp.MatchString("a([a-z]+)e", "apple")
+	fmt.Println(match)
+
+	r := regexp.MustCompile("a([a-z]+)e")
+	ms := r.MatchString("apple")
+	fmt.Println(ms)
+
+	r2 := regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+	fs := r2.FindString("/view/test")
+	fmt.Println(fs)
+	//r2から検索対象と結果のスライスを作る
+	fs2 := r2.FindStringSubmatch("/view/test")
+	fmt.Println(fs2, fs2[0], fs2[1], fs2[2])
+	//マッチしない文字列を与えるとエラーになる？？
+	//fs2 = r2.FindStringSubmatch("/data/test")
+	fmt.Println(fs2, fs2[0], fs2[1], fs2[2])
+}
