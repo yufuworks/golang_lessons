4a7918ecd5e3ec46c23fc81d44cfd656d188a076
yufuworks <yufuworks@outlook.jp>
2023-04-15 23:17:24 +0900

    ss9: Web Applicationを作成する 76. Web Applications 3 - templateとhttp.ResponseWriterとhttp.Request

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -26,18 +27,35 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	// /view/test のうちtest部分のみを切り出して、それにマッチするファイルを読み込む
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	_, err := fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	err := t.Execute(w, p)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// /view/test のうちtest部分のみを切り出して、それにマッチするファイルを読み込む
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	// エラーがあった場合でもeditパスに流す？？
+	if err != nil {
+		fmt.Println(err)
+		//p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	// サーバー起動、問題があればログ出力するのでこのように書ける。この状態で動かすとlocalhostでwebサーバーが立ち上がる。
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
