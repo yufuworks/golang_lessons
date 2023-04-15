4dbe76be556fde8459e27d9fddefcaf7040398fd
yufuworks <yufuworks@outlook.jp>
2023-04-16 00:23:50 +0900

    ss9: Web Applicationを作成する 78. Web Applications 5 - templateのキャッシュとハンドラー

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 type Page struct {
@@ -27,17 +27,19 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// 読み込んだtxtファイル（ページ）をキャッシングしておく
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	err := t.Execute(w, p)
+	// キャッシュに用意したtemplatesに対してExecuteTemplateすることでファイルを毎回読み込まなくて済む
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
-		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	// /view/test のうちtest部分のみを切り出して、それにマッチするファイルを読み込む
-	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -45,9 +47,11 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
+	// rを使ってないと注意されたのでトリあずログに出してみた
+	path := r.URL.Path
+	log.Printf("path: %v\n", path)
 	// エラーがあった場合でもeditパスに流す？？
 	if err != nil {
 		// もし存在しないページにアクセスしてもeditページに進むことでその後保存すれば新しいページとして保存される。
@@ -56,8 +60,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -69,10 +72,27 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+// パスのバリデーション、実装したページ三つのパスを判定する
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(w http.ResponseWriter, r *http.Request, string string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// リクエストパスをバリデーションにかける、/区切りのスライスになる。
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		log.Printf("m type:%T value:%v m2:%v\n", m, m, m[2])
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		// mの2つめの要素が各Handler関数の第三引数、title strin になる想定
+		fn(w, r, m[2])
+	}
+}
+
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	// サーバー起動、問題があればログ出力するのでこのように書ける。この状態で動かすとlocalhostでwebサーバーが立ち上がる。
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
