93d03fdd638b89b93ee55d3b2440fcac9cee0dc9
yufuworks <yufuworks@outlook.jp>
2023-04-15 20:53:09 +0900

    ss9: Web Applicationを作成する 74. Web Applications 1 -ioutil

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "awesomeProject/mylib/lessons/s8"
+import (
+	"fmt"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
 
 func main() {
-	s8.Main06()
+	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
+	if err := p1.save(); err != nil {
+		fmt.Println(err)
+	}
+
+	p2, _ := loadPage(p1.Title)
+	fmt.Println(string(p2.Body))
 }
